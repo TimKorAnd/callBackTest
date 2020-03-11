6c5b34a10b2bfcc5c179b925620897b47a5c8b0a
TimKorAnd <timkorand@gmail.com>
2020-03-11 21:18:51 +0200

    error check

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 function loadScript(src,callback) {
     let script = document.createElement('script');
     script.src = src;
-    script.onload = () => callback(script)
+    script.onload = () => callback(null, script)
+    script.onerror = () => callback(new Error(`Error script: /n ${script} /n loading.`))
     document.head.append(script);
 }
 
-loadScript('./loadedScript.js', (script) => {
+loadScript('./loadedScript.js', (error, script) => {
+    if (error) {
+
+    }
     newFunction(script.src)
     loadScript('./loadedScript2.js', () => {
         newFunction2('kuku2')
